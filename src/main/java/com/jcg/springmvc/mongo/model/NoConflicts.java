5b63294a12b42a38916723d0ee711e9a21e97811
AnnaLikhachova <a.cannsh@gmail.com>
2020-01-14 17:31:28 +0200

    This is commit from 14.01.20

--- src/main/java/com/jcg/springmvc/mongo/model/NoConflicts.java
+++ src/main/java/com/jcg/springmvc/mongo/model/NoConflicts.java
@@ -1,31 +1,23 @@
 package com.jcg.springmvc.mongo.model;
 
+import java.util.Date;
+import java.util.Objects;
+
 public class NoConflicts {
     private String id;
     private String number;
     private String name;
     private int grow;
+    private String aim;
+    private Date timestamp;
+    private int counter;
 
-    public NoConflicts(String id, String number, String name) {
-        this.id = id;
-        this.number = number;
-        this.name = name;
+    public int getCounter() {
+        return counter;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 
     public String getName() {
@@ -36,7 +28,52 @@ public class NoConflicts {
         this.name = name;
     }
 
+    public int getGrow() {
+        return grow;
+    }
+
+    public void setGrow(int grow) {
+        this.grow = grow;
+    }
+
+    public String getAim() {
+        return aim;
+    }
+
+    public void setAim(String aim) {
+        this.aim = aim;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
     public void saveChanges(int counter){
-        this.grow += counter;
+        this.grow += counter - 90;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        NoConflicts that = (NoConflicts) o;
+        return grow == that.grow &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(aim, that.aim) &&
+                Objects.equals(timestamp, that.timestamp);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, grow, aim, timestamp);
+    }
+
+    public void weMakeSomeConflicts(){
+        System.out.println("Here exists conflict");
     }
 }
+
